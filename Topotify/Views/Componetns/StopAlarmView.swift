ba3f4daac6e0325b7099fa871db3ebc388ee3028
usamaeltmsah <usamaeltmsah@gmail.com>
2022-12-20 12:55:33 +0200

    Add gesture to the stop alarm button

--- Topotify/Views/Componetns/StopAlarmView.swift
+++ Topotify/Views/Componetns/StopAlarmView.swift
@@ -9,50 +9,73 @@ import SwiftUI
 
 struct StopAlarmView: View {
     var color: Color = .black
+    @State private var buttonOffset = CGSize.zero
+    let buttonWidth: CGFloat = 75
+    
     var body: some View {
-        ZStack {
-            Capsule()
-                .fill(color.opacity(0.2))
-                .frame(height: 80)
-            
-            Capsule()
-                .fill(color.opacity(0.2))
-                .frame(height: 60)
-                .padding()
-            
-            Text("Stop Alarm")
-                .font(.system(size: 30, design: .rounded))
-                .fontWeight(.bold)
-                .foregroundColor(.yellow)
-            
-            HStack {
-//                Capsule()
-//                    .fill(.yellow)
-//                    .frame(width: 50, height: 50)
-                Spacer()
+        GeometryReader { geometry in
+            ZStack {
+                Capsule()
+                    .fill(color.opacity(0.2))
+                    .frame(height: 100)
                 
-            }
-            
-            HStack {
-                ZStack {
-                    Circle()
+                Capsule()
+                    .fill(color.opacity(0.4))
+                    .frame(height: 80)
+                    .padding(10)
+                
+                Text("Stop Alarm")
+                    .font(.system(size: 30, design: .rounded))
+                    .fontWeight(.bold)
+                    .foregroundColor(.yellow)
+                    .opacity(abs(buttonOffset.width) > 0 ? 0 : 1 )
+                
+                HStack {
+                    Capsule()
                         .fill(.yellow)
+                        .frame(width: buttonWidth + buttonOffset.width, height: buttonWidth)
+                        .padding()
                     
-                    Circle()
-                        .fill(.black.opacity(0.2))
-                        .padding(8)
-                    
-                    Image(systemName: "chevron.right.2")
-                        .font(.system(size: 20, weight: .bold, design: .rounded))
-                    
+                    Spacer()
                 }
-                .frame(width: 70, alignment: .leading)
-                .padding(5)
                 
-                Spacer()
+                HStack {
+                    ZStack {
+                        Circle()
+                            .fill(.yellow)
+                        
+                        Circle()
+                            .fill(.black.opacity(0.2))
+                            .padding(8)
+                        
+                        Image(systemName: "chevron.right.2")
+                            .font(.system(size: 20, weight: .bold, design: .rounded))
+                        
+                    }
+                    .frame(width: buttonWidth, alignment: .leading)
+//                    .padding(15)
+                    
+                    Spacer()
+                }
+                .padding(15)
+                .offset(x: buttonOffset.width)
+                .gesture(DragGesture()
+                    .onChanged({ gesture in
+                        if (gesture.translation.width <= geometry.size.width - (buttonWidth + 60) && gesture.translation.width >= 0) {
+                            buttonOffset = gesture.translation
+                        }
+                    })
+                    .onEnded({ _ in
+                        withAnimation(.easeOut(duration: 0.5)) {
+                            if (buttonOffset.width <= geometry.size.width - (buttonWidth + 70)) {
+                                buttonOffset = .zero
+                            }
+                        }
+                    })
+                )
             }
+            .padding(10)
         }
-        .padding(.horizontal, 10)
     }
 }
 
