35876e3d31770786d8fa8a139e9c9f5b2fa16ff6
usamaeltmsah <usamaeltmsah@gmail.com>
2023-01-21 22:22:41 +0200

    Add Extension to show or hide tab bar

--- Topotify/Extentions/View+Extension.swift
+++ Topotify/Extentions/View+Extension.swift
@@ -5,4 +5,56 @@
 //  Created by Usama Fouad on 07/01/2023.
 //
 
-import Foundation
+import SwiftUI
+
+extension View {
+    /// Hide or show the view based on a boolean value.
+    ///
+    /// Example for visibility:
+    ///
+    ///     Text("Label")
+    ///         .isHidden(true)
+    ///
+    /// Example for complete removal:
+    ///
+    ///     Text("Label")
+    ///         .isHidden(true, remove: true)
+    ///
+    /// - Parameters:
+    ///   - hidden: Set to `false` to show the view. Set to `true` to hide the view.
+    ///   - remove: Boolean value indicating whether or not to remove the view.
+    @ViewBuilder func isHidden(_ hidden: Bool, remove: Bool = false) -> some View {
+        if hidden {
+            if !remove {
+                self.hidden()
+            }
+        } else {
+            self
+        }
+    }
+}
+
+
+struct ShowTabBar: ViewModifier {
+    func body(content: Content) -> some View {
+        return content.padding(.zero).onAppear {
+            Tool.showTabBar()
+        }
+    }
+}
+struct HiddenTabBar: ViewModifier {
+    func body(content: Content) -> some View {
+        return content.padding(.zero).onAppear {
+            Tool.hiddenTabBar()
+        }
+    }
+}
+
+extension View {
+    func showTabBar() -> some View {
+        return self.modifier(ShowTabBar())
+    }
+    func hiddenTabBar() -> some View {
+        return self.modifier(HiddenTabBar())
+    }
+}
