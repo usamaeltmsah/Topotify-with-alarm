54cf6df15aac46d709801bc0f265e45eb9de18a1
usamaeltmsah <usamaeltmsah@gmail.com>
2023-01-21 22:35:27 +0200

    Make a component for the Search Bar view

--- Topotify/Views/Componetns/SearchBarView.swift
+++ Topotify/Views/Componetns/SearchBarView.swift
@@ -8,13 +8,41 @@
 import SwiftUI
 
 struct SearchBarView: View {
+    @Binding var searchText: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Image(systemName: "magnifyingglass")
+                .foregroundColor (
+                    searchText.isEmpty ? Color.secondary : Color.white
+                )
+            TextField("Search by track name", text: $searchText)
+                .foregroundColor (Color.white)
+                .overlay(
+                    Image (systemName: "xmark.circle.fill")
+                        .padding ()
+                        .offset(x: 10)
+                        .foregroundColor (Color.white)
+                        .opacity(searchText.isEmpty ? 0.0 : 1.0)
+                        .onTapGesture {
+                            searchText = ""
+                        }
+                    ,alignment: .trailing
+                )
+        }
+        .font(.headline)
+        .padding ()
+        .background(
+            RoundedRectangle (cornerRadius: 25)
+                .fill(Color(.darkBlueColor))
+                .shadow(color: Color.primary.opacity(0.15), radius: 10, x: 0, y: 0)
+        )
+        .padding()
     }
 }
 
 struct SearchBarView_Previews: PreviewProvider {
     static var previews: some View {
-        SearchBarView()
+        SearchBarView(searchText: .constant(""))
     }
 }
