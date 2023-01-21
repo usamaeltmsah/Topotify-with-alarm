ee5823c78c47f665a052ec1589312064e61bcac8
usamaeltmsah <usamaeltmsah@gmail.com>
2023-01-21 22:22:13 +0200

    Add Extension to show or hide tab bar

--- Topotify/Extentions/UIView+Extensions.swift
+++ Topotify/Extentions/UIView+Extensions.swift
@@ -5,4 +5,33 @@
 //  Created by Usama Fouad on 13/01/2023.
 //
 
-import Foundation
+import UIKit
+
+extension UIView {
+    func allSubviews() -> [UIView] {
+        var res = self.subviews
+        for subview in self.subviews {
+            let riz = subview.allSubviews()
+            res.append(contentsOf: riz)
+        }
+        return res
+    }
+}
+
+struct Tool {
+    static func showTabBar() {
+        UIApplication.shared.keyWindow?.windowScene?.windows.first(where: { $0.isKeyWindow })?.allSubviews().forEach({ (v) in
+            if let view = v as? UITabBar {
+                view.isHidden = false
+            }
+        })
+    }
+    
+    static func hiddenTabBar() {
+        UIApplication.shared.keyWindow?.windowScene?.windows.first(where: { $0.isKeyWindow })?.allSubviews().forEach({ (v) in
+            if let view = v as? UITabBar {
+                view.isHidden = true
+            }
+        })
+    }
+}
