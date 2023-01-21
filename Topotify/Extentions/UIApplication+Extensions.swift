702a2a76edab71c9c677bce1dfea81aeafc72761
usamaeltmsah <usamaeltmsah@gmail.com>
2023-01-21 22:21:15 +0200

    get keyWindow from UIApplication

--- Topotify/Extentions/UIApplication+Extensions.swift
+++ Topotify/Extentions/UIApplication+Extensions.swift
@@ -5,4 +5,21 @@
 //  Created by Usama Fouad on 13/01/2023.
 //
 
-import Foundation
+import UIKit
+
+extension UIApplication {
+    
+    var keyWindow: UIWindow? {
+        // Get connected scenes
+        return UIApplication.shared.connectedScenes
+        // Keep only active scenes, onscreen and visible to the user
+            .filter { $0.activationState == .foregroundActive }
+        // Keep only the first `UIWindowScene`
+            .first(where: { $0 is UIWindowScene })
+        // Get its associated windows
+            .flatMap({ $0 as? UIWindowScene })?.windows
+        // Finally, keep only the key window
+            .first(where: \.isKeyWindow)
+    }
+    
+}
